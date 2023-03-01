68a52de955d8e1074724615a79c296732d31f6da
chahn <chahn970@gmail.com>
2023-02-28 20:06:17 -0700

    add gallery js

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,14 @@
 function activateGallery() {
-	alert("hello from the gallery file");
+	let thumbnails = document.querySelectorAll('#gallery-thumbs > div > img');
+	let mainImage = document.querySelector('#gallery-photo > img');
+	thumbnails.forEach(function(thumbnail){
+		thumbnail.addEventListener('click', function(){
+			// set clicked image as main image
+			let newImageSrc = thumbnail.dataset.largeVersion;
+			mainImage.setAttribute('src', newImageSrc);
+			mainImage.setAttribute('alt', thumbnail.alt);
+		});
+	})
+
+
 }
