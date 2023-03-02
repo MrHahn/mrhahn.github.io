0db3cd69e0291c9b91be8cdb07c30c8fa417d482
chahn <chahn970@gmail.com>
2023-03-01 20:09:42 -0700

    finish the javascript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,7 @@
+//Activates the image gallery.
+//The main task is to attach an event listener to each image in the gallery
+//and respond appropriately to the click
+
 function activateGallery() {
 	let thumbnails = document.querySelectorAll('#gallery-thumbs > div > img');
 	let mainImage = document.querySelector('#gallery-photo > img');
@@ -7,6 +11,19 @@ function activateGallery() {
 			let newImageSrc = thumbnail.dataset.largeVersion;
 			mainImage.setAttribute('src', newImageSrc);
 			mainImage.setAttribute('alt', thumbnail.alt);
+
+			// change which image is current.
+			let currentClass = "current"
+			document.querySelector('.current').classList.remove(currentClass);
+			thumbnail.parentNode.classList.add(currentClass);
+
+			// update image info
+			let galleryInfo = document.querySelector('#gallery-info');
+			let title = galleryInfo.querySelector('.title');
+			let description = galleryInfo.querySelector('.description');
+
+			title.innerHTML = thumbnail.dataset.title;
+			description.innerHTML = thumbnail.dataset.description;
 		});
 	})
 
